bc748dc4f5db0a89bf38c8a1971fb0fc141e6283
Joshua Emore <ehimareemore@gmail.com>
2023-03-28 04:32:23 -0700

    Prints out a character

---
+++ print_c.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdarg.h>
+
+/**
+ * print_c - This function prints out a character
+ * and is linked to _prinf.c file through structure
+ * @c: character to print from va_list object
+ * Return: 1 i.e success
+ */
+int print_c(va_list c)
+{
+	char print_char = (char)va_arg(c, int);
+
+	printf("%c", print_char);
+	return (1);
+}
